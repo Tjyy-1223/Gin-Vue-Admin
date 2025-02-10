c5d5d2c1b1eadc77e8f72f9b0f416771c0925bbe
tianjiangyu <161840237@nuaa.edu.cn>
2025-02-10 18:21:28 +0800

    log

---
+++ gin-blog-admin/src/views/log/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <CommonPage>
+        <NResult class="mt-30" status="404" title="该页面还在开发中..." description="稍安毋躁" size="huge" />
+    </CommonPage>
+</template>
+
+<script setup>
+import { NResult } from 'naive-ui'
+import CommonPage from '@/components/common/CommonPage.vue'
+</script>
+
+<style lang="scss" scoped></style>
